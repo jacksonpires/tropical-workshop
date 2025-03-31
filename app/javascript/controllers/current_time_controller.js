7952b5ac6e1b9f37266482338fc28b9a65411a46
Jackson Pires <jackson.pires@gmail.com>
2025-03-31 20:20:46 -0300

    hotwire: atualizando a hora automaticamente usando targets

--- app/javascript/controllers/current_time_controller.js
+++ app/javascript/controllers/current_time_controller.js
@@ -2,8 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="current-time"
 export default class extends Controller {
+  static targets = [ "currentTimeDiv" ]
+
   connect() {
     const currentTime = new Date().toLocaleTimeString('pt-BR', { hour12: false })
     this.element.textContent = `Hora atual: ${currentTime}`
+
+    setInterval(() => {
+      const currentTime = new Date().toLocaleTimeString('pt-BR', { hour12: false })
+      this.currentTimeDivTarget.textContent = `Hora atual: ${currentTime}`
+    }
+    , 1000)
   }
 }
