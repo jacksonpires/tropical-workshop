24890d05ea42dc77b599cc7f0e5a51ff34c022c5
Jackson Pires <jackson.pires@gmail.com>
2025-03-31 20:51:19 -0300

    phlex: usando hotwire + phlex

--- app/views/phlex_example/current_date_and_time.rb
+++ app/views/phlex_example/current_date_and_time.rb
@@ -1,9 +1,16 @@
 class Views::PhlexExample::CurrentDateAndTime < Views::Base
   def view_template
-    div(class: "p-4 border rounded-md") do
+    div(
+      class: "p-4 border rounded-md",
+      data: {
+        controller: "current-time",
+        current_time_pre_text_value: "TROPICAL INFORMA ->"
+      }
+    ) do
       p { "Data atual: #{Date.today.strftime('%d/%m/%Y')}" }
-      p { "Hora atual (24H): #{Time.now.strftime('%H:%M:%S')}" }
-      p { "Hora atual (12H): #{Time.now.strftime('%I:%M:%S')}" }
+
+      p(data: { current_time_target: "currentTime24" }) { "Hora atual (24H): #{Time.now.strftime('%H:%M:%S')}" }
+      p(data: { current_time_target: "currentTime12" }) { "Hora atual (12H): #{Time.now.strftime('%I:%M:%S')}" }
     end
   end
 end
