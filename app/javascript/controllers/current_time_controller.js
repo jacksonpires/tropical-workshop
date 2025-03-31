a3579739b1effbc51df0756c7c8327610da6a46a
Jackson Pires <jackson.pires@gmail.com>
2025-03-31 20:25:20 -0300

    hotwire: refatora para usar funcao

--- app/javascript/controllers/current_time_controller.js
+++ app/javascript/controllers/current_time_controller.js
@@ -5,19 +5,19 @@ export default class extends Controller {
   static targets = [ "currentTime24", "currentTime12" ]
 
   connect() {
-    const localeTime24 = new Date().toLocaleTimeString('pt-BR', { hour12: false })
-    const localeTime12 = new Date().toLocaleTimeString('pt-BR', { hour12: true })
-
-    this.currentTime24Target.textContent = `Hora atual (24H): ${localeTime24}`
-    this.currentTime12Target.textContent = `Hora atual (12H): ${localeTime12}`
+    this.#setCurrentTime()
 
     setInterval(() => {
-      const localeTime24 = new Date().toLocaleTimeString('pt-BR', { hour12: false })
+      this.#setCurrentTime()
+    }
+    , 1000)
+  }
+
+  #setCurrentTime() {
+    const localeTime24 = new Date().toLocaleTimeString('pt-BR', { hour12: false })
       const localeTime12 = new Date().toLocaleTimeString('pt-BR', { hour12: true })
 
       this.currentTime24Target.textContent = `Hora atual (24H): ${localeTime24}`
       this.currentTime12Target.textContent = `Hora atual (12H): ${localeTime12}`
-    }
-    , 1000)
   }
 }
