6195d6cda24fe6f89226e7bce17d035e506c77d3
Jackson Pires <jackson.pires@gmail.com>
2025-03-31 20:45:47 -0300

    phlex: renderizando direto do controller

---
+++ app/views/phlex_example/index.rb
@@ -0,0 +1,14 @@
+class Views::PhlexExample::Index < Views::Base
+  def initialize(name:)
+    @name = name
+  end
+
+  def view_template
+    div(class: "flex w-full justify-center") do
+      div(class: "flex flex-col bg-gray-100 p-10 gap-4 items-center shadow-md rounded-md") do
+        h1(class: "text-4xl text-purple-700 font-bold") { "Hello, #{@name}" }
+        a(href: "https://www.phlex.fun/introduction/", class: "text-purple-500", target: "_blank") { "Phlex Docs" }
+      end
+    end
+  end
+end
