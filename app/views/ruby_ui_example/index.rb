8b3f89485bd2f694ed6ad212dc6af87d6c9e96bc
Jackson Pires <jackson.pires@gmail.com>
2025-03-31 21:21:47 -0300

    rubyui: adicionando exemplo alert dialog

--- app/views/ruby_ui_example/index.rb
+++ app/views/ruby_ui_example/index.rb
@@ -2,11 +2,27 @@ module Views
   module RubyUiExample
     class Index < Views::Base
       def view_template
-        div(class: "flex w-full justify-center") do
+        div(class: "flex flex-col items-center w-full justify-center gap-4") do
           Alert do
             lucide_icon("rocket")
             AlertTitle { "Pro tip" }
             AlertDescription { "Simply, don't include an icon and your alert will look like this." }
+          end
+
+          AlertDialog do
+            AlertDialogTrigger do
+              Button { "Show dialog" }
+            end
+            AlertDialogContent do
+              AlertDialogHeader do
+                AlertDialogTitle { "Are you absolutely sure?" }
+                AlertDialogDescription { "This action cannot be undone. This will permanently delete your account and remove your data from our servers." }
+              end
+              AlertDialogFooter do
+                AlertDialogCancel { "Cancel" }
+                AlertDialogAction { "Continue" } # Will probably be a link to a controller action (e.g. delete account)
+              end
+            end
           end
         end
       end
